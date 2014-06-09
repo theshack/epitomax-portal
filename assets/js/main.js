ed25954bfa64e2aa4cb7e81e28157d514b2a2df4
Matt Graham <graham@github.com>
2014-06-09 15:29:04 -0400

    adding config.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,14 @@
 $(function(){
-  var companyId = "135";
-  var epitomax_url = "http://my.epitomax.net/PatientPortal/signup.jsp?company_id=" + companyId + "&case_no=";
+  var epitomax_url = "http://my.epitomax.net/PatientPortal/signup.jsp?company_id=" + config.company_id + "&case_no=";
   $("form").submit(function(e){
     e.preventDefault();
     window.location.href = epitomax_url + $("#clientid").val();
   });
+
+  // Site Config Variables
+
+  $("title").prepend(config.name + " ");
+  $(".business-name").html(config.name);
+  $(".business-phone").attr("href" , "tel:" + config.phone)
+                      .html(config.phone);
 });
---
+++ config.js
@@ -0,0 +1,5 @@
+config = {
+  name:"Renewal Christian Counseling Center, LLC.",
+  phone:"810-966-0099",
+  company_id:"135"
+}
